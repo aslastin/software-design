f5941aa09a417034fc6d670bc7b466d097b903e9
Aleksandr Slastin <a.slastin@bk.ru>
2022-12-12 05:50:11 +0300

    hw6 done

--- hw6/src/main/java/ru/aslastin/Main.java
+++ hw6/src/main/java/ru/aslastin/Main.java
@@ -1,7 +1,42 @@
 package ru.aslastin;
 
+import ru.aslastin.token.Token;
+import ru.aslastin.tokenizer.Tokenizer;
+import ru.aslastin.visitor.CalcVisitor;
+import ru.aslastin.visitor.ParserVisitor;
+import ru.aslastin.visitor.PrintVisitor;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 1) {
+            System.out.println("expected single argument: infix expression");
+            return;
+        }
+
+        String expression = args[0];
+        InputStream expressionStream = new ByteArrayInputStream(expression.getBytes(StandardCharsets.UTF_8));
+
+        Tokenizer tokenizer = new Tokenizer(expressionStream);
+        List<Token> tokenizedExpression = tokenizer.tokenizeExpression();
+
+        var parserVisitor = new ParserVisitor();
+        parserVisitor.visitAll(tokenizedExpression);
+        List<Token> parsedExpression = parserVisitor.getParsedExpression();
+
+        var printVisitor = new PrintVisitor(new ByteArrayOutputStream());
+        printVisitor.visitAll(parsedExpression);
+
+        var calcVisitor = new CalcVisitor();
+        calcVisitor.visitAll(parsedExpression);
+
+        System.out.println("Input expression: " + expression);
+        System.out.println("Reverse Polish notation: " + printVisitor.getOutputStream().toString());
+        System.out.println("Calculated result = " + calcVisitor.getResult());
     }
 }
