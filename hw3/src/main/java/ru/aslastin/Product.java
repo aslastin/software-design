cf68b2bcea7d5c60dbd29619b0e9a95eab68d598
Aleksandr Slastin <a.slastin@bk.ru>
2023-01-15 22:00:50 +0300

    hw3: add product class

---
+++ hw3/src/main/java/ru/aslastin/Product.java
@@ -0,0 +1,38 @@
+package ru.aslastin;
+
+import java.util.Objects;
+
+public class Product {
+    private final String name;
+    private final long price;
+
+    public Product(String name, long price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
