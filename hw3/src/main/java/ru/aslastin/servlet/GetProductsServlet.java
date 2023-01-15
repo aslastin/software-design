9dd0658c24394b5307da129b333889891e6c5c3c
Aleksandr Slastin <a.slastin@bk.ru>
2023-01-15 23:43:23 +0300

    hw3: update GetProductsServlet

--- hw3/src/main/java/ru/aslastin/servlet/GetProductsServlet.java
+++ hw3/src/main/java/ru/aslastin/servlet/GetProductsServlet.java
@@ -1,43 +1,21 @@
 package ru.aslastin.servlet;
 
+import ru.aslastin.processor.product.GetProductsRequestProcessor;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
-/**
- * @author akirakozov
- */
 public class GetProductsServlet extends HttpServlet {
+    private final GetProductsRequestProcessor requestProcessor;
+
+    public GetProductsServlet(String databasePath) {
+        requestProcessor = new GetProductsRequestProcessor(databasePath);
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try {
-            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                Statement stmt = c.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
-                response.getWriter().println("<html><body>");
-
-                while (rs.next()) {
-                    String  name = rs.getString("name");
-                    int price  = rs.getInt("price");
-                    response.getWriter().println(name + "\t" + price + "</br>");
-                }
-                response.getWriter().println("</body></html>");
-
-                rs.close();
-                stmt.close();
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
+        requestProcessor.process(request, response);
     }
 }
